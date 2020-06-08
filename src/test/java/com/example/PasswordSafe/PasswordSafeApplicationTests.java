d687949a70b26cdeb5292209482ff26c0509a819
Marco Sancassani <5ia17masancassani@gibwintra.ch>
2020-06-08 13:51:23 +0200

    first commit

---
+++ src/test/java/com/example/PasswordSafe/PasswordSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PasswordSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordSafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
